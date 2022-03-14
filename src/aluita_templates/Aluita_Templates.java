881253d8fdc5661697fbb31ce6d87ffe615129eb
Luan <lujetix@gmail.com>
2022-03-14 18:31:21 -0300

    added main of default importation config

--- src/aluita_templates/Aluita_Templates.java
+++ src/aluita_templates/Aluita_Templates.java
@@ -2,7 +2,6 @@ package aluita_templates;
 
 import Robo.AppRobo;
 import aluita_templates.Model.CriarExtratoBanco;
-import fileManager.Selector;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -10,20 +9,37 @@ import java.util.List;
 
 public class Aluita_Templates {
 
+    /**
+     * Processo para cada banco:
+     *  - Extrair dados do arquivo OFX
+     *  - Dentro da pasta do contas a pagar:
+     *    - Percorre todos os filtros de arquivos de contas a pagar
+     *      - Percorre todos os arquivos de contas a pagar naquele filtro
+     *        - Percorre todas as linhas do arquivo
+     *          - Se na coluna 'banco' existir o nome do banco, então:
+     *            - Cria um objeto do tipo LctoTemplate
+     */ 
+
     public static Integer empresa = 613;
 
     public static void main(String[] args) {
         AppRobo app = new AppRobo("Aluita Templates");
         
-        //app.setArquivoParametros(new File("C:/Users/TI01/Desktop/pp.cfg"));
-        //app.setLocalRetorno(new File("C:/Users/TI01/Desktop"));
-        
         try {
             app.definirParametros();
+
+            System.out.println(app.getParametro("mes"));
+            System.out.println(app.getParametro("ano"));
+
+            
+            //Convert 'mes' and 'ano' to integer
+            Integer mes = Integer.parseInt(app.getParametro("mes"));
+            Integer ano = Integer.parseInt(app.getParametro("ano"));
+
             app.executar(
                     principal(
-                            Integer.getInteger(app.getParametro("mes")),
-                            Integer.getInteger(app.getParametro("ano"))
+                            mes,
+                            ano                    
                     )
             );   
         }catch(Exception e){
@@ -41,10 +57,15 @@ public class Aluita_Templates {
                 //Define pastas
                 String pastaEmpresa = "Aluita Aluminio Porto Alegre Ltda";
                 File pastaEscrituraçãoMensal = new File("\\\\HEIMERDINGER\\docs\\Contábil\\Clientes\\" + pastaEmpresa + "\\Escrituração mensal");
+                
+                //Pasta de Extratos
                 File pastaExtratos = new File(pastaEscrituraçãoMensal.getAbsolutePath() + "\\" + ano + "\\Extratos\\" + mesMM + "." + ano);
+                //Pasta de Movimentos
+                File pastaMovimentos = new File(pastaEscrituraçãoMensal.getAbsolutePath() + "\\" + ano + "\\Movimento\\" + mesMM + "." + ano);
+
                 File pastaBancos = new File(pastaExtratos.getAbsolutePath() + "\\Bancos");
-                File pastaRetornos = new File(pastaExtratos.getAbsolutePath() + "\\Retorno");
-                File pastaPagamentos = new File(pastaExtratos.getAbsolutePath() + "\\Pagamentos\\PAGFOR");
+                File pastaRetornos = new File(pastaMovimentos.getAbsolutePath() + "\\Retorno");
+                File pastaPagamentos = new File(pastaMovimentos.getAbsolutePath() + "\\Pagamentos\\PAGFOR");
 
                 CriarExtratoBanco.setPastaEscrituraçãoMensal(pastaEscrituraçãoMensal);
                 CriarExtratoBanco.setPastaBancos(pastaBancos);
@@ -57,6 +78,8 @@ public class Aluita_Templates {
                     List<CriarExtratoBanco> listaExtratos = new ArrayList<>();
 
                     listaExtratos.add(new CriarExtratoBanco(empresa, mes, ano, "POA", "POA", 9, 1, true));
+                    listaExtratos.add(new CriarExtratoBanco(empresa, mes, ano, "CAXIAS", "CAXIAS", 9, 1, false));
+                    listaExtratos.add(new CriarExtratoBanco(empresa, mes, ano, "NH", "NH", 9, 1, false));
                     listaExtratos.add(new CriarExtratoBanco(empresa, mes, ano, "PELOTAS", "PELOTAS", 5008, 2, false));
                     listaExtratos.add(new CriarExtratoBanco(empresa, mes, ano, "PASSO FUNDO", "P;FUNDO", 2111, 4, false));
                     listaExtratos.add(new CriarExtratoBanco(empresa, mes, ano, "SANTA MARIA", "S;MARIA", 2315, 5, false));
